9cce2ffbf7723058741f8e9f54fb2d226f01dc13
s2lky <juj120856@gmail.com>
2023-11-23 15:34:07 +0900

    version: 1.01

--- app.py
+++ app.py
@@ -5,20 +5,12 @@ import os
 
 
 if __name__ == "__main__":
-    pid = os.getpid()
-    py = psutil.Process(pid)
-    memory_info = py.memory_info()
-    print(f"현재 메모리 사용량: {memory_info.rss / 1024 / 1024:.2f} MB")
-    print(f"가상 메모리 사용량: {memory_info.vms / 1024 / 1024:.2f} MB")
     try:
         bot = YouTubeBot()
         result_df = bot.run()  # Save the returned DataFrame as a variable
-        pid = os.getpid()
-        py = psutil.Process(pid)
-        memory_info = py.memory_info()
-        print(f"현재 메모리 사용량: {memory_info.rss / 1024 / 1024:.2f} MB")
-        print(f"가상 메모리 사용량: {memory_info.vms / 1024 / 1024:.2f} MB")
         bot.cleanup()
+        table_name = "crawl" 
+        insert_db(df=result_df, table_name=table_name)
         print("Script executed successfully.")
     except YouTubeBotError as e:
         print(f"Custom error raised: {e}")
